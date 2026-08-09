c181a436148e147ea544a6054c1c205d0675c662
trapcat1440 <lesterfigueroa90@gmail.com>
2026-08-09 02:38:51 -0700

    Update stripe.ts

--- lib/stripe.ts
+++ lib/stripe.ts
@@ -2,6 +2,5 @@ import "server-only"
 
 import Stripe from "stripe"
 
-export const stripe = new Stripe("pk_live_51TL7Q537M7PzuUPokRe5p5hiKF9hQ8rVn5QfjTigrG48hfwCPCl3LNvwi19mQlHiKrZ43FLrbU961qXbE8FBdGkf00vQMT2qrq")
-
+export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string)
 
